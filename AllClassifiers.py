a6120a15be5b0640abbe44ab0a22cb9c54f0a9d3
AAYUSH\Aayush Bhandari <aayuk.93@gmail.com>
2017-11-17 18:11:02 -0600

    Calcualting average Accuracy and Precision

--- AllClassifiers.py
+++ AllClassifiers.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import KFold
 from sklearn import metrics
 import  numpy as np
 import warnings
-
+import pandas as pd
 with warnings.catch_warnings():
         # warnings.filterwarnings("ignore")
     warnings.simplefilter("ignore")
@@ -19,12 +19,12 @@ with warnings.catch_warnings():
     classfiers = [tree.DecisionTreeClassifier(),linear_model.Perceptron(),neural_network.MLPClassifier(),svm.SVC(),naive_bayes.MultinomialNB(),
                   linear_model.LogisticRegression() ,neighbors.KNeighborsClassifier(), ensemble.BaggingClassifier(),ensemble.RandomForestClassifier(),
                   ensemble.AdaBoostClassifier(),ensemble.GradientBoostingClassifier()]
+
     classfiers_names = ["DecisionTreeClassifier","Perceptron","MLPClassifier","SVC()","MultinomialNB", "LogisticRegression" ,"KNeighborsClassifier",
                         "BaggingClassifier","RandomForestClassifier", "AdaBoostClassifier","GradientBoostingClassifier"]
 
     accuracy = {}
     precision = {}
-    import pandas as pd
 
     input_dataset = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/balance-scale/balance-scale.data",header=None)
     input_dataset[0] = input_dataset[0].astype('category')
@@ -32,17 +32,16 @@ with warnings.catch_warnings():
     Y = input_dataset.pop(0)
     X = input_dataset
 
+    # Converting dataset of list
     X = X.values.tolist()
     Y = Y.values.tolist()
 
+    # Converting list to numpy array
     X = np.array(X)
     Y = np.array(Y)
-    #print(X)
-    kf = KFold(n_splits=2)
 
+    kf = KFold(n_splits=10)
     for train_index, test_index in kf.split(X):
-        print("Train: ", len(train_index) , "Test: ", len(test_index))
-        #print(train_index,test_index)
         X_train,X_test = X[train_index],X[test_index]
         Y_train, Y_test = Y[train_index], Y[test_index]
         i=0
@@ -57,6 +56,13 @@ with warnings.catch_warnings():
                 precision[classfiers_names[i]].append(metrics.precision_score(Y_test, prediction, average='weighted'))
             i = i+1
 
-    print(accuracy)
-    print(precision)
-    
+    # print(accuracy)
+    # print(precision)
+
+    avgAccuracy = {}
+    avgPrecision = {}
+    for key in accuracy:
+        avgAccuracy[key] = format(np.mean(accuracy[key]),".2f")
+        avgPrecision[key] = format(np.mean(precision[key]),".2f")
+    print(avgAccuracy)
+    print(avgPrecision)
